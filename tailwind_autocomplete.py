e49b611fdc0cd28310b171b172f9ddc915990e0f
neog4f1 <37392219+neog4f1@users.noreply.github.com>
2022-12-05 08:30:34 +0700

    Update tailwind_autocomplete.py

--- tailwind_autocomplete.py
+++ tailwind_autocomplete.py
@@ -7,13 +7,13 @@ class tailwindCompletions(sublime_plugin.EventListener):
     def __init__(self):
         self.class_completions = [("%s \tTailwind Class" % s, s) for s in tailwind_classes]
         
-    def plugin_loaded(self):
+    def onLoad(self, view):
+        print view.fileName(), "just got loaded"
         self.settings = sublime.load_settings("tailwind_autocomplete.sublime-settings")
         print("setting:")
         print(self.settings.get("tailwindClasses"))
 
     def on_query_completions(self, view, prefix, locations):
-        print(self.settings.get("tailwindClasses"))
         setting = sublime.load_settings("tailwind_autocomplete.sublime-settings")
         jsSources = setting.get("scopes")
         # jsSources = ["source.tsx string.quoted", "text.html string.quoted"]
